0d670720c39d16736d7cdf4c3d836a5015602a05
dinnnzzz <cimolmanol@gmail.com>
2025-10-07 13:14:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const tombolSaapa = document.getElementById('sapaButton'):
+const tombolSapa = document.getElementById('sapaButton'):
 tombolSapa.addEventListener('click;function(){alert('Hayyyy!Thank you for click!'):}):
